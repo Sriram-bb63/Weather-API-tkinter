3a67599b840edbf376238f3c1306578faafdbc91
Sriram <71959217+Sriram-bb63@users.noreply.github.com>
2021-05-22 10:42:25 +0530

    Update weatherAPI tkinter.py

--- weatherAPI tkinter.py
+++ weatherAPI tkinter.py
@@ -1,61 +1,66 @@
-#importing modules
+# Importing modules
 from tkinter import Tk, Label
 import json
 import requests
 
-#creating window
-root=Tk()
+# Creating window
+root = Tk()
 root.title("Weather")
-root.geometry("250x70")
+# root.geometry("165x70")
 
-#display func
-def disp(APIcity,APItime,APIaqi,APIstatus,color):
+# Display func
+def disp(APIcity, APIaqi, APIstatus, color):
     root.configure(bg=color)
-    #Labels
-    city=Label(root, text=APIcity, bg=color, font=("Arial Bold", 10))
+    # Labels
+    city = Label(root, text=APIcity, bg=color, font=("Arial Bold", 10))
     city.grid(row=0, column=0)
 
-    time=Label(root, text="Time:"+str(APItime)+" hour", bg=color)
-    time.grid(row=0, column=1)
-
-    aqi=Label(root, text="AQI:"+str(APIaqi), bg=color, font=("Arial Bold", 10))
+    aqi = Label(root, text="AQI:"+str(APIaqi), bg=color, font=("Arial Bold", 10))
     aqi.grid(row=1, column=0, columnspan=3)
 
-    status=Label(root, text=APIstatus, bg=color)
+    status = Label(root, text=APIstatus, bg=color)
     status.grid(row=2, column=0, columnspan=3)
 
+
 try:
-    #getting API from AirNow
-    APIfile=requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=B9B759FC-6C22-496B-BEE2-2025217AE65C")
+    # getting API from AirNow
+    APIfile = requests.get(
+        'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode={zipcode}&distance=25&API_KEY={apikey}'.format(
+        zipcode='20002', apikey='B9B759FC-6C22-496B-BEE2-2025217AE65C')
+        )
+    print(">>>>>>>>>>", APIfile)
+    print(">>>>>>>>>>", type(APIfile))
+    print(">>>>>>>>>>", type(APIfile.content))
 
-    #get data from JSON
-    data=json.loads(APIfile.content)
-    APIcity=data[1]["ReportingArea"]
-    APItime=data[1]["HourObserved"]
-    APIaqi=data[1]["AQI"]
-    APIstatus=data[1]["Category"]["Name"]
+    # Get data from JSON
+    data = json.loads(APIfile.content)
+    print(">>>>>>>>>>",data)
+    print(">>>>>>>>>>", type(data))
+    APIcity = data[1]["ReportingArea"]
+    APIaqi = data[1]["AQI"]
+    APIstatus = data[1]["Category"]["Name"]
 
-    #setting bg color
-    if APIstatus=="Good":
-        color="green"
-    elif APIstatus=="Moderate":
-        color="yellow"
-    elif APIstatus=="Unhealthy for Sensitive Groups":
-        color="orange"
-    elif APIstatus=="Unhealthy":
-        color="red"
-    elif APIstatus=="Very Unhealthy":
-        color="purple"
+    # setting bg color
+    if APIstatus == "Good":
+        color = "green"
+    elif APIstatus == "Moderate":
+        color = "yellow"
+    elif APIstatus == "Unhealthy for Sensitive Groups":
+        color = "orange"
+    elif APIstatus == "Unhealthy":
+        color = "red"
+    elif APIstatus == "Very Unhealthy":
+        color = "purple"
     else:
-        color="maroon"
+        color = "maroon"
 
-    #display data
-    disp(APIcity,APItime,APIaqi,APIstatus,color)
-    
+    # display data
+    disp(APIcity, APIaqi, APIstatus, color)
+
 except:
     root.configure(bg="red")
-    error=Label(root, text="Error", bg="red")
+    error = Label(root, text="Error", bg="red")
     error.grid(row=0, column=1, columnspan=3)
 
-#mainlooping window
+# mainlooping window
 root.mainloop()
